0efc6e029a3e3a2f8515dab1afea3cd6be950680
Christy Yau <abstruse.amaranth@gmail.com>
2019-10-11 11:55:36 -0700

    Added second page and explicit intent that grabs search bar contents

--- app/src/main/java/ca/bcit/handypark/MainActivity.java
+++ app/src/main/java/ca/bcit/handypark/MainActivity.java
@@ -2,7 +2,12 @@ package ca.bcit.handypark;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnSearch = findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(btnLstnr);
     }
+
+    private View.OnClickListener btnLstnr = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            EditText etDestination = findViewById(R.id.etDestination);
+            String dest = etDestination.getText().toString();
+            // Explicit intent
+            Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
+            intent.putExtra("dest", "You searched for " + dest);
+            startActivity(intent);
+        }
+    };
 }
