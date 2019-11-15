ac60158b30a0ebd82ec64d763d34a6963937484a
Christy Yau <abstruse.amaranth@gmail.com>
2019-11-15 14:17:40 -0800

    Dynamically generate URL based on geolocation, destination, and parking meter selected

--- app/src/main/java/ca/bcit/handypark/ParkingDetails.java
+++ app/src/main/java/ca/bcit/handypark/ParkingDetails.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -12,7 +13,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,6 +25,8 @@ public class ParkingDetails extends AppCompatActivity {
     Parking parkingSpot;
     ListView lvResults;
     double[] destCoords = new double[2];
+    String destName = "";
+    String googleMapsUrl = "";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,20 +35,21 @@ public class ParkingDetails extends AppCompatActivity {
         lvResults = findViewById(R.id.lvResults);
         Intent intent = getIntent();
         Bundle args = intent.getBundleExtra("BUNDLE");
-        destCoords = intent.getDoubleArrayExtra("DESTINATION");
+        destCoords = intent.getDoubleArrayExtra("DESTCORDS");
+        destName = intent.getStringExtra("DESTNAME");
         parkingSpots = (ArrayList<Parking>) args.getSerializable("ARRAYLIST");
         Collections.sort(parkingSpots,  Parking.COMPARE_BY_DISTANCE);
 
         lvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(ParkingDetails.this, MapsActivity.class);
-                Bundle args = new Bundle();
-                args.putSerializable("ARRAYLIST", parkingSpots);
-                intent.putExtra("INDEX",i);
-                intent.putExtra("BUNDLE",args);
-                intent.putExtra("DESTINATION",destCoords);
-                startActivity(intent);
+                // Assign dynamically generated URL to googleMapsURL
+                googleMapsUrl = urlBuilder(i);
+
+                Uri gmmIntentUri = Uri.parse(googleMapsUrl);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
             }
         });
 
@@ -54,6 +60,34 @@ public class ParkingDetails extends AppCompatActivity {
             ParkingAdapter adapter = new ParkingAdapter(ParkingDetails.this, parkingSpots);
             lvResults.setAdapter(adapter);
         }
+    }
+
+    /**
+     * Dynamically generates URL based on three parameters:
+     * 1. User's geolocation (automatically retrieved as origin is left blank)
+     * 2. Parking meter selected in ParkingDetails activity, and
+     * 3. Destination entered into search bar in MainActivity.
+     * @param index as int (Index of the ListView item clicked)
+     * @return url as String
+     */
+    public String urlBuilder(int index) {
+        String baseUrl = getString(R.string.baseUrl);
+
+        String destNameSnippet = destName.replace(" ", "+");
+        String destUrl = getString(R.string.destUrl);
+
+        //Split parking location into words
+        String[] parkingLocationArray = parkingSpots.get(index).getLocation().split(" ");
+        //Trim off "North / South / East / West Side" in front of location
+        String[] parkingLocationSubArray = Arrays.copyOfRange(parkingLocationArray, 3,
+                parkingLocationArray.length);
+        //Turn location back into String
+        StringBuilder location = new StringBuilder();
+        for (String str : parkingLocationSubArray) {
+            location.append(str + " ");
+        }
+
+        return baseUrl + location + destUrl + destNameSnippet;
     }
 
 }
